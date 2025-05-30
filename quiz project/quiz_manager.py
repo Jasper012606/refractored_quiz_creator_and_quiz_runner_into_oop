d2f39b0fa1ffc0bb00e391b94e285ae7411d27b0
Jasper <japeemga@gmail.com>
2025-05-30 15:08:31 +0800

    Adjusted some parts and made it a little more readable

--- quiz project/quiz_manager.py
+++ quiz project/quiz_manager.py
@@ -1,9 +1,10 @@
 import os
 from questions import Question
 
-class QuizManager:
+class QuizManager(Question):
     def __init__(self, file_name = "quiz.txt"):
         self.file_name = file_name
+        super().__init__()
         
     def question_exists(self, question):
         if not os.path.exists(self.file_name):
@@ -11,6 +12,7 @@ class QuizManager:
         with open(self.file_name, 'r') as file:
             return any(question.lower() in line.lower() for line in file)
         
-    def save_question(self, question):
-        with open(self.file_name, "a") as file:
-            file.write(question.format_question())  
+    def save_question(self):
+        if not self.question_exists():    
+            with open(self.file_name, "a") as file:
+                file.write(self.format_question())  
