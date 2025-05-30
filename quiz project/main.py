7918996dfcefe248dfc0d51b99beb56cf8c08cd4
Jasper <japeemga@gmail.com>
2025-05-30 21:21:35 +0800

    Print options for the user

---
+++ quiz project/main.py
@@ -0,0 +1,7 @@
+from quiz_creator import QuizCreator
+from quiz_loader import QuizLoader
+from quiz_game import QuizGame
+
+def main():
+    print("Welcome to the Quiz Application!\n 1. Create a Quiz\n 2. Load a Quiz\n 3. Exit")
+    
