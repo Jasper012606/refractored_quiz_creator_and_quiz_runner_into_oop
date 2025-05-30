c6752400d59d9dc9b440971bb91b1a7e319107b2
Jasper <japeemga@gmail.com>
2025-05-30 20:51:53 +0800

    Get the questions, choices, and the correct answer from the text/quiz file

--- quiz project/quiz_loader.py
+++ quiz project/quiz_loader.py
@@ -10,3 +10,23 @@ class QuizLoader:
             print(f"No quiz file found. Please create a quiz first.")
             return []
         
+        with open(self.file_name, "r") as file:
+            lines = [lines.strip() for lines in file.readlines() if lines.strip()]
+            
+        questions = []
+        number_of_line = 0
+        
+        while number_of_line < len(lines):
+            if lines[number_of_line].startswith("Question:"):
+                question = lines[number_of_line].replace("Question: ", "").strip()
+                choices = {}
+                for j in range(1, 5):
+                    label, choice = lines[number_of_line + j].split(". ", 1)
+                    choices[label.strip()] = choice.strip()
+                correct_line = lines[number_of_line + 5]
+                correct_label = correct_line.split(":")[1].split(".")[0].strip()
+                questions.append(Question(question, choices, correct_label))
+                number_of_line += 6
+            else:
+                number_of_line += 1
+        return questions
